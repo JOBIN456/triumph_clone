76ca8ab8a9a03defd75ca0d439b95cf5fb5e7f4f
JOBIN456 <jobinj5210@gmail.com>
2024-02-19 17:02:21 +0530

    feb 20

--- trumphapp/context_processors.py
+++ trumphapp/context_processors.py
@@ -14,11 +14,21 @@ def navbar_data(request):
 
     rocket3_category = Category.objects.get(name='ROCKET3')
     rocket3_bikes = Bike.objects.filter(category=rocket3_category)
+    stealth_category = Category.objects.get(name='STEALTH_EDITION')
+    stealth_bikes = Bike.objects.filter(category=stealth_category)
+    sport_category = Category.objects.get(name='SPORT')
+    sport_bikes = Bike.objects.filter(category=sport_category)
+    chrome_category = Category.objects.get(name='CHROME COLLECTION')
+    chrome_bikes = Bike.objects.filter(category= chrome_category)
+
 
     return {
         "adventure_bikes": adventure_bikes,
         "classics_bikes": classics_bikes,
         "roadster_bikes": roadster_bikes,
         "rocket3_bikes": rocket3_bikes,
+        "stealth_bikes":stealth_bikes,
+        "sport_bikes":sport_bikes,
+        "chrome_bikes":chrome_bikes,
     }
 
