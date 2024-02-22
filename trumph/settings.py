393cf29e55d1292facc1e159475bc8037ada7515
JOBIN456 <jobinj5210@gmail.com>
2024-02-22 11:13:59 +0530

    feb

--- trumph/settings.py
+++ trumph/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
+STATIC_URL = '/static/'
 if not DEBUG:
         STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
         STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
