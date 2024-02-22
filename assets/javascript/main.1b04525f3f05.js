66b7ca255a54baf661b786afc2353edc54a71d74
JOBIN456 <jobinj5210@gmail.com>
2024-02-22 11:45:34 +0530

    feb

---
+++ assets/javascript/main.1b04525f3f05.js
@@ -0,0 +1,19 @@
+function zoomIn() {
+    document.getElementById('zoomImage').classList.add('zoomed');
+}
+function zoomOut() {
+    document.getElementById('zoomImage').classList.remove('zoomed');
+}
+
+
+// function toggleDropdown(dropdown) {
+//     dropdown.classList.toggle('active');
+//   }
+
+
+
+    // function toggleAdventureInfo() {
+    //     const adventureInfo = document.querySelector('.adventure-info');
+    //     adventureInfo.classList.toggle('show');
+    // }
+
