6be2f3cbe499d7de93694b8d6b265d418ee088f2
JOBIN456 <jobinj5210@gmail.com>
2024-02-22 12:55:03 +0530

    FEB

--- trumph/settings.py
+++ trumph/settings.py
@@ -127,7 +127,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-MEDIA_ROOT="Truimph/trumph/media"
+MEDIA_ROOT="/Truimph/trumph/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
