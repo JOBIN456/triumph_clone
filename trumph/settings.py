26d3b2feb7b58acb02a8b5aeba4376eafeb18d24
JOBIN456 <jobinj5210@gmail.com>
2024-02-22 14:39:03 +0530

    feb

--- trumph/settings.py
+++ trumph/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -126,8 +126,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
-# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-# MEDIA_ROOT="/Truimph/trumph/media"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
