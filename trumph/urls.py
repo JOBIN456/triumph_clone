665baade721fdde0f011592932b765168765dc79
JOBIN456 <jobinj5210@gmail.com>
2024-02-22 13:30:21 +0530

    feb

--- trumph/urls.py
+++ trumph/urls.py
@@ -18,7 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings 
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('',include("trumphapp.urls"))
 ]
@@ -29,5 +34,5 @@ if settings.DEBUG:
                document_root=settings.MEDIA_ROOT)
 
 
-if not settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if not settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
