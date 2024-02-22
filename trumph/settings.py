5027c0d27333e5e3e0b98e92365132201658cf93
JOBIN456 <jobinj5210@gmail.com>
2024-02-22 10:46:55 +0530

    GG

--- trumph/settings.py
+++ trumph/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-es5h(#)@&cy&f1t!zgfa+c#3q+(!hac@!z4=3tue63*k%(xomm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
