900123c6dd4c9a54d2ecd5db8d1fd6c4a9491408
JOBIN456 <jobinj5210@gmail.com>
2024-02-23 11:23:17 +0530

    feb

--- trumph/urls.py
+++ trumph/urls.py
@@ -19,7 +19,6 @@ from django.urls import path,include, re_path
 from django.conf.urls.static import static
 from django.conf import settings 
 from django.views.static import serve
-from django.conf.urls import url
 urlpatterns = [
 
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
